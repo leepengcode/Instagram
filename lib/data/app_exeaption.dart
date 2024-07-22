4299a0b2ecb16d8e2b700f0dc5d58995afba1618
Lui Peng <norngliipeng98.info@gmail.com>
2024-07-23 00:44:15 +0700

    Update lesson

---
+++ lib/data/app_exeaption.dart
@@ -0,0 +1,23 @@
+class AppExeaption implements Exception {
+  late final _message;
+  late final _profix;
+  AppExeaption([this._message, this._profix]);
+
+  String toString() {
+    return '$_profix$_message';
+  }
+}
+
+class FetchDataException extends AppExeaption {
+  FetchDataException([String? message])
+      : super(message, 'Error During Communication');
+}
+
+class BadRequestException extends AppExeaption {
+  BadRequestException([String? message]) : super(message, 'Invalid request');
+}
+
+class UnaThorisedException extends AppExeaption {
+  UnaThorisedException([String? message])
+      : super(message, 'UnaThorised request');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,3 @@
-import 'package:demo_app_provider/Provider/count_provider.dart';
-import 'package:demo_app_provider/Provider/darkmode.dart';
-import 'package:demo_app_provider/Provider/favorite_provider.dart';
-import 'package:demo_app_provider/Provider/login_provider.dart';
-import 'package:demo_app_provider/Provider/slider_provider.dart';
-import 'package:demo_app_provider/Screen/DarkMode/darkmode.dart';
-import 'package:demo_app_provider/Screen/Favorite/favorite.dart';
-import 'package:demo_app_provider/Screen/login.dart';
-import 'package:demo_app_provider/Screen/provider_state_management.dart';
-import 'package:demo_app_provider/Screen/slider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,35 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => countProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => SliderProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => FavoriteProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => themeModeProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => LoginProvider(),
-          )
-        ],
+        providers: [],
         child: Builder(builder: (BuildContext context) {
-          final themeChanger = Provider.of<themeModeProvider>(context);
-
           return MaterialApp(
-            themeMode: themeChanger.themeMode,
             theme: ThemeData(
                 appBarTheme: AppBarTheme(backgroundColor: Colors.yellow)),
             darkTheme: ThemeData(
                 brightness: Brightness.dark,
                 appBarTheme: AppBarTheme(backgroundColor: Colors.lightBlue)),
             title: 'Flutter Demo',
-            home: LoginScreen(),
           );
         }));
   }
